74a18a58100b7a14ae9c66eaad360444a5eb90e5
Francesco Witte <github@witte.sh>
2023-03-19 23:27:39 +0100

    Work on compressed air example and fix some typos

--- book/single-components/utilities.py
+++ book/single-components/utilities.py
@@ -2,31 +2,17 @@ from CoolProp.CoolProp import PropsSI as PSI
 
 
 def calc_physical_exergy(p, h, p0, T0, fluid):
-    r"""
-    Calculate specific physical exergy.
+    r"""Calculate specific physical exergy."""
+    s = PSI("S", "P", p, "H", h, fluid)
+    h0 = PSI("H", "P", p0, "T", T0, fluid)
+    s0 = PSI("S", "P", p0, "T", T0, fluid)
 
-    Physical exergy is allocated to a thermal and a mechanical share.
+    ex = (h - h0) - T0 * (s - s0)
+    return ex
 
-    Parameters
-    ----------
-    p : float
-        Pressure of fluid p / Pa.
 
-    h : float
-        Spec. enthalpy of fluid T / K.
-
-    p0 : float
-        Ambient pressure p0 / Pa.
-
-    T0 : float
-        Ambient temperature T0 / K.
-
-    Returns
-    -------
-    e_ph : tuple
-        Specific thermal and mechanical exergy
-        (:math:`e^\mathrm{T}`, :math:`e^\mathrm{M}`) in J / kg.
-    """
+def calc_splitted_physical_exergy(p, h, p0, T0, fluid):
+    r"""Calculate specific physical exergy according to splitting rule."""
     s = PSI("S", "P", p, "H", h, fluid)
 
     h_T0_p = PSI("H", "P", p, "T", T0, fluid)
